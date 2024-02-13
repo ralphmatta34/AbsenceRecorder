1473f9d1d841294278bb8a80a68ccbf319f46aba
ralphmatta34 <matta.r@etoncollege.org.uk>
2024-02-13 19:20:56 +0100

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Ralph Matta on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
