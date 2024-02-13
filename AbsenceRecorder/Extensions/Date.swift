40e8ea4680567b2a261f379cc2700d979d8bf50e
ralphmatta34 <matta.r@etoncollege.org.uk>
2024-02-13 22:56:59 +0100

    Finished all tasks.

---
+++ AbsenceRecorder/Extensions/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  AbsenceRecorder
+//
+//  Created by Ralph Matta on 13/02/2024.
+//
+
+import Foundation
---
+++ AbsenceRecorder/View/DivisionItem.swift
@@ -0,0 +1,18 @@
+//
+//  DivisionItem.swift
+//  AbsenceRecorder
+//
+//  Created by Ralph Matta on 13/02/2024.
+//
+
+import SwiftUI
+
+struct DivisionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisionItem()
+}
